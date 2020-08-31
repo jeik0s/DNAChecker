f5d0f24b556f11078887179bf114147ddc0996b7
Jakub Zaremba <jzaremba@MAgiC.local>
2020-08-31 23:59:01 +0200

    Working Beta version 0.9.5

---
+++ index.php
@@ -0,0 +1,219 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+        body{
+            overflow-x: hidden;
+        }
+        .row{
+            padding-top: 1%;;
+            text-align: center;
+        }
+
+        .row .col-3{
+            height: 150px;
+            text-align: center;
+            vertical-align: middle;
+            line-height: 150px; 
+        }
+
+        .row .col-3:nth-child(1){
+            background-color: lightcyan;
+        }
+
+        .row .col-3:nth-child(2){
+            background-color:lightgrey;
+        }
+
+        .row .col-3:nth-child(3){
+            background-color:lightskyblue;
+        }
+
+        .row .col-3:nth-child(4){
+            background-color: lightyellow;
+        }
+
+        .leftside{
+            float: left;
+        }
+        #blueBold{
+            color: blue;
+        }
+        #redBold{
+            color: red;
+        }
+    </style>
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <div class="row">
+        <div class="col-3">All Generations</div>
+        <div class="col-3">All Ones Generations</div>
+        <div class="col-3">100%<</div>
+        <div class="col-3">General information</div>
+      </div>
+
+    <div class="row">
+        <div class="col-1"></div>
+        <div class="col-10">
+            <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+            Ilość prób
+            <br/>
+            <br/>
+ <table style="width:100%">
+      <tr>
+        <th>DNA diffrences</th>
+      </tr>
+      <tr>
+        <td id="badDNA"></td>
+      </tr>
+      <tr>
+        <td  id="goodDNA"></td>
+      </tr>
+    </table> 
+        <br />
+        <br />
+        <br />
+        <div class="leftside">Number of populations: <div id="NumberOfPopulation"></div></div>
+        <div class="leftside" style="margin-left: 50px;">Elements in populations: <div id="ElementsInPopulation"></div></div>
+        <div class="leftside" style="margin-left: 50px;">Number of generations below 100: <div id="belowOneHundred"></div></div>
+        <div class="leftside" style="margin-left: 50px;">Number of generations below 100 in %: <div id="belowOneHundredInPercentage"></div></div>
+        <div class="leftside" style="margin-left: 50px;">The lowest generation needed: 
+            <div id="TheLessNeededOne">
+            </div>
+             [ <i id="TheLessNeededOneIndex"></i> ]
+        </div>
+        
+        <div class="leftside" style="margin-left: 50px;">The higest generation needed: 
+            <div id="TheMostNeededOne">
+            </div>
+
+             [ <i id="TheMostNeededOneIndex"></i> ]
+
+        </div>
+
+
+        </div>
+        <div class="col-1"></div>
+    </div>  
+
+<script src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
+<script src="https://canvasjs.com/assets/script/jquery.canvasjs.min.js"></script>
+
+    <script>
+window.onload = function() {
+ 
+var dataPoints = [];
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    theme: "light2",
+    zoomEnabled: true,
+    title: {
+        text: "AI Generations"
+    },
+    axisY: {
+        title: "Number of generations",
+        titleFontSize: 24,
+        prefix: "G"
+    },
+    data: [{
+        type: "line",
+        yValueFormatString: "###",
+        dataPoints: dataPoints
+    }]
+});
+
+
+function findDiff(str1, str2){ 
+  let diff= "";
+  str2.split('').forEach(function(val, i){
+    if (val != str1.charAt(i))
+      diff += val ;         
+  });
+  return diff;
+}
+
+ 
+function addData(data) {
+
+
+
+    var gooddna = data["HealthyDNA"].split("");
+    var baddna = data["UnhealthyDNA"].split("");
+    var gooddnaString = ""
+    var baddnaString = ""
+
+    
+    for(i=0;i<gooddna.length;i++){
+        if(gooddna[i] != baddna[i]){
+            gooddna[i] = "<b id='blueBold'>"+gooddna[i]+"</b>"
+            baddna[i] = "<b id='redBold'>"+baddna[i]+"</b>"
+        }
+        gooddnaString = gooddna.join('')
+        baddnaString = baddna.join('')
+    }
+
+    document.getElementById("goodDNA").innerHTML = gooddnaString;
+    document.getElementById("badDNA").innerHTML = baddnaString;
+    belowOneHundred = 0;
+    TheMostNeededOne = 0;
+    belowOneHundredInPercentage = 0.0;
+    TheMostNeededOneIndex = 0;
+    TheLessNeededOne = 1000;
+    TheLessNeededOneIndex = 1000;
+
+    document.getElementById("NumberOfPopulation").innerHTML = data["NumberOfPopulation"];
+    document.getElementById("ElementsInPopulation").innerHTML = data["ElementsInPopulation"];
+    var dps = data["Data"];
+    for (var i = 0; i < data["NumberOfPopulation"]; i++) {
+    // console.log(dps[i]["0"]["Generation"])
+    if(dps[i]["0"]["Generation"] < 101)
+        belowOneHundred++;
+
+    if(TheMostNeededOne < dps[i]["0"]["Generation"]){
+        TheMostNeededOne = dps[i]["0"]["Generation"]
+        TheMostNeededOneIndex = i;
+    }
+
+
+    if(TheLessNeededOne > dps[i]["0"]["Generation"]){
+        TheLessNeededOne = dps[i]["0"]["Generation"]
+        TheLessNeededOneIndex = i;
+    }
+
+        dataPoints.push({
+            x: i,
+            y: dps[i]["0"]["Generation"]
+        });
+    }
+
+    belowOneHundredInPercentage = belowOneHundred / data["NumberOfPopulation"] * 100
+
+    document.getElementById("belowOneHundred").innerHTML = belowOneHundred;
+    document.getElementById("TheMostNeededOne").innerHTML = TheMostNeededOne;
+    document.getElementById("TheLessNeededOne").innerHTML = TheLessNeededOne;
+    console.log(TheLessNeededOneIndex)
+    console.log(TheMostNeededOneIndex)
+    document.getElementById("TheLessNeededOneIndex").innerHTML = TheLessNeededOneIndex;
+    document.getElementById("TheMostNeededOneIndex").innerHTML = TheMostNeededOneIndex;
+    document.getElementById("belowOneHundredInPercentage").innerHTML = belowOneHundredInPercentage + "%";
+    chart.render();
+}
+ 
+
+$.getJSON("output.json", addData);
+
+ 
+}
+    </script>
+
+</body>
+</html>
---
+++ runDNA.py
@@ -0,0 +1,74 @@
+from genetic_library import GeneticAlgorithm, Element
+from genetic_library.selection_models import elite_selection_model
+
+from random import randint, choice
+
+# For testing: https://www.bioinformatics.org/sms2/random_dna.html
+
+HealthDNA = "tattatcttctagcgtatggatactttgtcgctaccaatagaccgggctagactcgccgatgagtcggttagacggtgcacccagattattacacttaac"
+TARGET =    "tattatcttctagcgtatggatactttgtcgctaccaatagaccgggctaggtacgccgatgagtcggttagacggtgcacccagattattacacttaac"
+Population = 100
+trys = 150;
+
+class DNAsequence(Element):
+    POSSIBILITIES = '''agtc'''
+
+    def __init__(self, DNAsequence):
+        self.DNAsequence = DNAsequence
+        super().__init__()
+
+    def _perform_mutation(self):
+        random_index = randint(0, len(self.DNAsequence) - 1)
+        DNAsequence_as_list = list(self.DNAsequence)
+        DNAsequence_as_list[random_index] = choice(self.POSSIBILITIES)
+        self.DNAsequence = "".join(DNAsequence_as_list)
+
+    def crossover(self, element2: 'Element' ) -> 'Element':
+        length = int(randint(0, len(self.DNAsequence) - 1))
+        new_DNAsequence = self.DNAsequence[:length] + element2.DNAsequence[length:]
+
+        return DNAsequence(new_DNAsequence)
+
+    def evaluate_function(self):
+        diff = 0
+        for letter1, letter2 in zip(self.DNAsequence, TARGET):
+            if letter1 != letter2:
+                diff += 1
+        return diff
+
+    def __repr__(self):
+        return self.DNAsequence
+
+
+def first_population_generator():
+    return [DNAsequence(HealthDNA) for _ in range(Population)]
+
+
+def stop_condition(string, current_fitness, i):
+    return current_fitness == 0
+
+whole_process = '''{
+    "HealthyDNA":"''' + str(HealthDNA) + '''",
+    "UnhealthyDNA": "''' + str(TARGET) + '''",
+    "ElementsInPopulation": "''' + str(Population) + '''",
+    "NumberOfPopulation": "''' + str(trys) + '''",
+    "Data":{
+    '''
+next_try = 0;
+
+for i in range(trys):
+    json_file = '"'+str(next_try)+'":' + '{'
+    ga = GeneticAlgorithm(first_population_generator, elite_selection_model, stop_condition)
+    for result in ga.run():
+        json_file = json_file + result + ","
+    json_file = json_file[:-1]
+    json_file = json_file + "}"
+    # print(json_file)
+    whole_process = whole_process + json_file + ",";
+    next_try = next_try + 1;
+whole_process = whole_process[:-1]
+whole_process = whole_process + "}}"
+
+file = open("output.json","w+")
+file.write(whole_process)
+file.close;
